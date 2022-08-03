3c64fdd0004f572dcf9856f6a20ff614bd097891
Mohammad Saleh Rostami <62114872+Saleh-Programmer@users.noreply.github.com>
2022-08-03 17:41:37 +0430

    Add files via upload

---
+++ Python Course Mr Ordokhani TopLearn/1to15summary.py
@@ -0,0 +1,27 @@
+# history
+    # 1985 to 1991 
+    # khido fun roson (holand)
+
+# usage
+    # in every field from web to ML to AL to Game to hack and more
+
+# basic
+    # indenting -> any indent in python is meanigful
+    # its easy to learn
+    # its powerful
+    # Visual studio code and Parcharm are best for coding
+    # It's case sensetive
+
+# sintax
+print("For Print Somthing in terminal we use print() function")
+input("To get input in terminal we use input() function :  ")
+
+# variable naming -> start with charecter or _ and only use number , _ , Charcter
+_variable = 10
+variable_ =10 
+_1variable = 10
+print(_variable,_1variable,variable_)
+# python supports dynamicaly variable --> means we can change either variable amount and type 
+
+
+
